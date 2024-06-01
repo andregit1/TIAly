2e2dacad6bce9a19e086ab1cd2cb360a0cb369e5
andregit1 <andreaswidjaya1@gmail.com>
2024-06-01 23:42:10 +0700

    update config/db for connect in docker

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const development = {
   database: `${process.env.POSTGRES_DB}`,
   username: `${process.env.POSTGRES_USER}`,
   password: `${process.env.POSTGRES_PASSWORD}`,
+  host: `${process.env.DB_HOST}` || "localhost",
   dialect: 'postgres',
   dialectOptions: {
     // pg options here
